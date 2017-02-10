d0147217d4cbc400a89026976d93b5a05f93e79a
SurapatWai <wa.surapat_st@tni.ac.th>
2017-02-10 04:19:47 +0000

    Q2b

--- test.py
+++ test.py
@@ -7,12 +7,23 @@ import datetime
 datetime.datetime.now().isoformat()
 mynums = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
 index = 6;
+## ANSWER Q1.a
 print(mynums[6]);
+## ANSWER Q1.b
 for i in mynums:
     if(i<1):
         print(i)
+## ANSWER Q2.a
 list = []
 for  i in range(-13,14):
     list.append(i)
     
 print(list)
+
+## ANSWER Q2.b
+##
+from random import randint 
+list2 = []
+for i in range(20):
+    list2.append(randint(1,12))
+print(list2)
