bc9c20dae5505f3a8f4879e687f0a96732ee53d5
SurapatWai <wa.surapat_st@tni.ac.th>
2017-02-10 04:11:10 +0000

    Q2a

--- test.py
+++ test.py
@@ -8,3 +8,9 @@ datetime.datetime.now().isoformat()
 mynums = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
 index = 6;
 print(mynums[6]);
+
+list = []
+for  i in range(-13,14):
+    list.append(i)
+    
+print(list)
